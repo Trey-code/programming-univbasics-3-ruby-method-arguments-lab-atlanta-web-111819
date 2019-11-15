1eacfbb35809c7f70015b79491dfc949e7e898e0
Treven Womack <keytonwomack@yahoo.com>
2019-11-15 16:14:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,11 @@ puts "Hi, my name is #{name}."
 end
 introduction "Trey"
 
-
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning how to program in #{language}."
+end
+  introduction_with_language "Trey, Ruby"
+  
 
 
 
